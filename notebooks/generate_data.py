553da4523bb4778bf49681ffc1d532b77ddcd7e1
Cheikh Mbacké Diene <cheikhmbackediene@esp.sn>
2026-04-03 02:05:20 +0000

    Lab 1 : structure projet + dataset + exploration

---
+++ notebooks/generate_data.py
@@ -0,0 +1,42 @@
+﻿import pandas as pd
+import numpy as np
+import os
+
+os.makedirs("data", exist_ok=True)
+
+np.random.seed(42)
+n = 500
+
+diagnostics = np.random.choice(
+    ["sain", "paludisme", "grippe", "typhoide"],
+    n,
+    p=[0.316, 0.272, 0.260, 0.152]
+)
+
+temperature = []
+for d in diagnostics:
+    if d == "grippe":
+        temperature.append(round(np.random.normal(38.5, 0.5), 1))
+    elif d == "paludisme":
+        temperature.append(round(np.random.normal(39.4, 0.4), 1))
+    elif d == "typhoide":
+        temperature.append(round(np.random.normal(39.0, 0.4), 1))
+    else:
+        temperature.append(round(np.random.normal(36.8, 0.3), 1))
+
+df = pd.DataFrame({
+    "age":           np.random.randint(18, 70, n),
+    "sexe":          np.random.choice(["M", "F"], n),
+    "temperature":   temperature,
+    "tension_sys":   np.random.randint(10, 14, n),
+    "toux":          np.random.randint(0, 2, n),
+    "fatigue":       np.random.randint(0, 2, n),
+    "maux_tete":     np.random.randint(0, 2, n),
+    "courbatures":   np.random.randint(0, 2, n),
+    "frissons":      np.random.randint(0, 2, n),
+    "perte_appetit": np.random.randint(0, 2, n),
+    "diagnostic":    diagnostics,
+})
+
+df.to_csv("data/patients_dakar.csv", index=False)
+print("Dataset cree : 500 patients dans data/patients_dakar.csv")
